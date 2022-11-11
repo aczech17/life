c35133cfa84a68e23d1c8495cb96a0284d461171
aczech17 <adrian-czech@outlook.com>
2022-11-11 16:46:02 +0100

    Update cell_matrix.h

--- cell_matrix.h
+++ cell_matrix.h
@@ -11,5 +11,5 @@ typedef struct Cell_matrix
 Cell_matrix init_random_cell_matrix(int width, int height);
 Cell_matrix init_cell_matrix(FILE* file);
 void evolve(Cell_matrix*);
-void print_cell_matrix(Cell_matrix);
+void print_cell_matrix(const Cell_matrix*);
 void delete_cell_matrix(Cell_matrix*);
